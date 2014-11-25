d42e89c60bd3ca26222287b9bc0fd76d734571ec
Ryan C. Boykin <ryan@theyapps.com>
2014-11-24 20:29:31 -0600

    Refactored game mode switch

--- js/Tron.js
+++ js/Tron.js
@@ -55,65 +55,78 @@ function update() {
  * Render screen
  */
 function render() {
-    if(game_state === RUNNING){
-        ctx.fillStyle = EMPTY_COLOR;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    switch (game_state){
+        case RUNNING:
+            ctx.fillStyle = EMPTY_COLOR;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the grid
-        for(var x = 0; x < GRID_SIZE; x++) {
-            for (var y = 0; y < GRID_SIZE; y++) {
-                switch (grid.get_cell(x, y)) {
-                    case EMPTY:
-                        ctx.fillStyle = EMPTY_COLOR;
-                        break;
-                    case player:
-                        ctx.fillStyle = player.color;
-                        break;
+            // Draw the grid
+            for(var x = 0; x < GRID_SIZE; x++) {
+                for (var y = 0; y < GRID_SIZE; y++) {
+                    switch (grid.get_cell(x, y)) {
+                        case EMPTY:
+                            ctx.fillStyle = EMPTY_COLOR;
+                            break;
+                        case player:
+                            ctx.fillStyle = player.color;
+                            break;
+                    }
+                    ctx.fillRect(x * CELL_SIZE,y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                 }
-                ctx.fillRect(x * CELL_SIZE,y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             }
-        }
-        /*
-        ctx.fillStyle = "#fff";
-        ctx.fillText(
-            "player.x: " + player.x +
-            " player.y: " + player.y,
-            10, GRID_SIZE * CELL_SIZE - 10);
-         */
+            /*
+             ctx.fillStyle = "#fff";
+             ctx.fillText(
+             "player.x: " + player.x +
+             " player.y: " + player.y,
+             10, GRID_SIZE * CELL_SIZE - 10);
+             */
+            break;
+        case WIN:
+            ctx.fillStyle = "rgba(225,225,225,0.2)";
+            ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
+            ctx.fillStyle = "#000";
+            ctx.textAlign = "center";
+            ctx.textBaseline="middle";
+            ctx.fillText(
+                "YOU WIN!!",
+                (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2);
+            break;
+        case LOSE:
+            ctx.fillStyle = "rgba(225,225,225,0.2)";
+            ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
+            ctx.fillStyle = "#000";
+            ctx.textAlign = "center";
+            ctx.textBaseline="middle";
+            ctx.font="30px Arial";
+            ctx.fillText(
+                "YOU LOSE!!",
+                (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2);
+            break;
+        case PAUSED:
+            ctx.fillStyle = "rgba(225,225,225,0.2)";
+            ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
+            ctx.fillStyle = "#000";
+            ctx.textAlign = "center";
+            ctx.textBaseline="middle";
+            ctx.font="30px Arial";
+            ctx.fillText(
+                "Paused",
+                (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2);
+            break;
+            break;
+        default:
+            ctx.fillStyle = "#000";
+            ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
+            ctx.fillStyle = "#fff";
+            ctx.textAlign = "center";
+            ctx.textBaseline="middle";
+            ctx.font="30px Arial";
+            ctx.fillText(
+                "There has been an error.",
+                (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2);
+            break;
     }
-    else if(game_state === WIN){
-        ctx.fillStyle = "rgba(225,225,225,0.2)";
-        ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
-        ctx.fillStyle = "#000";
-        ctx.textAlign = "center";
-        ctx.textBaseline="middle";
-        ctx.fillText(
-            "YOU WIN!!",
-            (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2)
-    }
-    else if(game_state === LOSE){
-        ctx.fillStyle = "rgba(225,225,225,0.2)";
-        ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
-        ctx.fillStyle = "#000";
-        ctx.textAlign = "center";
-        ctx.textBaseline="middle";
-        ctx.font="30px Arial";
-        ctx.fillText(
-            "YOU LOSE!!",
-            (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2)
-    }
-    else{
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0,0,CELL_SIZE * GRID_SIZE, CELL_SIZE * GRID_SIZE);
-        ctx.fillStyle = "#fff";
-        ctx.textAlign = "center";
-        ctx.textBaseline="middle";
-        ctx.font="30px Arial";
-        ctx.fillText(
-            "There has been an error.",
-            (GRID_SIZE * CELL_SIZE)/2, (GRID_SIZE * CELL_SIZE)/2)
-    }
-
 }
 
 /**
